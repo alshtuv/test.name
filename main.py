c340721aa6f98536b530847a0ebd84ba58064d10
Nova <xyvjonathan@gmail.com>
2026-03-26 00:43:07 +0000

    4th commit message for the changes made to main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# a python code XD
+# a python code basic for me to remember :3
 
 # asks user for name
 print ("What's your name?")
 username = input("")
+
+# strips any whitespace from the inputted name
+username = username.strip()
+
+# Capiltizes name
+username = username.title()
+
+# checks if the inputted name is empty and prompts the user to enter a valid name
+while username == "":
+    print("Please enter a valid name.")
+    username = input("")
+    username = username.strip()
+
+#Splits name into first and last name
+first, last = username.split(" ")
+
 # prints a greeting message with the inputted name
-print("Hello, " + username + "! Welcome to the world!")
+print("Hello, " + first + "! Welcome to the world!")
+
